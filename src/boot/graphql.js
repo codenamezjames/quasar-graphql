e43562dff7f632ae86113a9a90a86be44919cce2
James Harrington <jharrington@neulioncollege.com>
2019-03-08 11:33:34 -0500

    Adding some error handeling oppertunities

--- src/boot/graphql.js
+++ src/boot/graphql.js
@@ -2,6 +2,8 @@ import { ApolloClient } from 'apollo-client'
 import VueApollo from 'vue-apollo'
 import { HttpLink } from 'apollo-link-http'
 import { InMemoryCache } from 'apollo-cache-inmemory'
+import { onError } from 'apollo-link-error'
+import { from } from 'apollo-link'
 
 export default ({ Vue, app }) => {
   const uri = process.env.API_END_POINT || 'http://localhost:3000/'
@@ -9,7 +11,21 @@ export default ({ Vue, app }) => {
 
   // Create the apollo client
   const defaultClient = new ApolloClient({
-    link: httpLink,
+    link: from([
+      onError(({ graphQLErrors, networkError }) => {
+        if (graphQLErrors) {
+          graphQLErrors.map(({ message, locations, path }) => {
+            // Handle Graph Error
+            // console.log(`[GraphQL error]: Message: ${message}, Location: ${locations}, Path: ${path}`)
+          })
+        }
+        if (networkError) {
+          // Handle Network Error
+          // console.log(`[Network error]: ${networkError}`)
+        }
+      }),
+      httpLink
+    ]),
     cache: new InMemoryCache()
   })
 
